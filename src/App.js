9fbd6f6c47a63c14592411560995975c283f3511
ernestomelchor <nurturelove88@gmail.com>
2020-08-07 15:47:00 -0500

    remove learn react line

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   const randomNumber = () => {
     return Math.random();
   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>Refresh this page to get a new random number each time:</p>
         {randomNumber()}
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        ></a>
       </header>
     </div>
   );
